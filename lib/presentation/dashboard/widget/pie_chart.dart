15b66a319366c3eb542cbda8edb119c838fa8b52
Anwar Chong C00404 <anwar.bujang@tmrnd.com.my>
2024-04-18 16:54:34 +0800

    feat:

--- lib/presentation/dashboard/widget/pie_chart.dart
+++ lib/presentation/dashboard/widget/pie_chart.dart
@@ -2,6 +2,7 @@ import 'package:budgetapp/app/app_style.dart';
 import 'package:budgetapp/common/utils/utils.dart';
 import 'package:budgetapp/common/viewmodel/tag/tag_viewmodel.dart';
 import 'package:budgetapp/common/viewmodel/transaction/transaction_viewmodel.dart';
+import 'package:budgetapp/common/widgets/icons/custom_icon.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -21,10 +22,81 @@ class _IBPieChartWidgetState extends ConsumerState<IBPieChartWidget> {
   Widget build(BuildContext context) {
     return ref.watch(transactionViewmodelProvider).when(
       data: (data) {
-        return Container(
-          height: 100,
-          color: getColorFromHex(
-              ref.read(tagViewmodelProvider.notifier).getTagColor(tagId: 1)),
+        return AspectRatio(
+          aspectRatio: 1.8,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: AspectRatio(
+                  aspectRatio: 2,
+                  child: PieChart(
+                    PieChartData(
+                      pieTouchData: PieTouchData(
+                        touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                          setState(() {
+                            if (!event.isInterestedForInteractions ||
+                                pieTouchResponse == null ||
+                                pieTouchResponse.touchedSection == null) {
+                              touchedIndex = -1;
+                              return;
+                            }
+                            touchedIndex = pieTouchResponse
+                                .touchedSection!.touchedSectionIndex;
+                          });
+                        },
+                      ),
+                      borderData: FlBorderData(
+                        show: false,
+                      ),
+                      sectionsSpace: 0,
+                      centerSpaceRadius: 40,
+                      startDegreeOffset: 180,
+                      sections: [
+                        for (int i = 0; i < data.length; i++)
+                          pieChartData(
+                              value: double.parse(data[i].transactionAmount),
+                              tagId: data[i].tagId,
+                              color: getColorFromHex(ref
+                                  .read(tagViewmodelProvider.notifier)
+                                  .getTagColor(tagId: data[i].tagId)),
+                              isTouched: touchedIndex == i),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  for (int i = 0; i <= 4; i++)
+                    if (i < 4)
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              data.elementAt(i).title,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            IBIcon(tagId: data.elementAt(i).tagId)
+                          ],
+                        ),
+                      ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text(
+                      '...Others',
+                      style: TextStyle(color: AppColors.offWhiteVariant),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         );
       },
       error: (error, stackTrace) {
@@ -34,91 +106,20 @@ class _IBPieChartWidgetState extends ConsumerState<IBPieChartWidget> {
         return Container();
       },
     );
-//     return AspectRatio(
-//       aspectRatio: 1.8,
-//       child: Row(
-//         children: <Widget>[
-//           Expanded(
-//             child: AspectRatio(
-//               aspectRatio: 2,
-//               child: PieChart(
-//                 PieChartData(
-//                   pieTouchData: PieTouchData(
-//                     touchCallback: (FlTouchEvent event, pieTouchResponse) {
-//                       setState(() {
-//                         if (!event.isInterestedForInteractions ||
-//                             pieTouchResponse == null ||
-//                             pieTouchResponse.touchedSection == null) {
-//                           touchedIndex = -1;
-//                           return;
-//                         }
-//                         touchedIndex = pieTouchResponse
-//                             .touchedSection!.touchedSectionIndex;
-//                       });
-//                     },
-//                   ),
-//                   borderData: FlBorderData(
-//                     show: false,
-//                   ),
-//                   sectionsSpace: 0,
-//                   centerSpaceRadius: 40,
-//                   startDegreeOffset: 180,
-//                   sections: [
-//                     for (int i = 0; i < temp.length; i++)
-//                       pieChartData(
-//                           value: 30,
-//                           category: temp.elementAt(i),
-//                           isTouched: touchedIndex == i),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ),
-//           Column(
-//             crossAxisAlignment: CrossAxisAlignment.end,
-//             mainAxisAlignment: MainAxisAlignment.end,
-//             children: [
-//               for (int i = 0; i <= 4; i++)
-//                 if (i < 4)
-//                   Padding(
-//                     padding: const EdgeInsets.all(4.0),
-//                     child: Row(
-//                       children: [
-//                         Text(
-//                           temp.elementAt(i).name,
-//                           style: TextStyle(color: Colors.white),
-//                         ),
-//                         SizedBox(
-//                           width: 5,
-//                         ),
-//                         // getCategoryIcon(category: temp.elementAt(i)),
-//                       ],
-//                     ),
-//                   ),
-//               Padding(
-//                 padding: const EdgeInsets.all(4.0),
-//                 child: Text(
-//                   '...Others',
-//                   style: TextStyle(color: AppColors.offWhiteVariant),
-//                 ),
-//               ),
-//             ],
-//           )
-//         ],
-//       ),
-//     );
-//
   }
 }
 
 PieChartSectionData pieChartData(
-    {required double value, required String tag, bool isTouched = false}) {
+    {required double value,
+    required int tagId,
+    required Color color,
+    bool isTouched = false}) {
   final fontSize = isTouched ? 25.0 : 16.0;
   final radius = isTouched ? 60.0 : 50.0;
   return PieChartSectionData(
       radius: radius,
       value: value,
-      // color: getCategoryColor(category: category),
+      color: color,
       titleStyle: TextStyle(
           color: AppColors.white,
           fontWeight: FontWeight.bold,
