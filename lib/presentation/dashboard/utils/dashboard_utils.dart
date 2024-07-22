c6c37935735e4bc67e86625a81530d124d3335ec
AnwarBujangHuat <anwarbinbujang@gmail.com>
2024-07-22 21:44:08 +0800

    📝

--- lib/presentation/dashboard/utils/dashboard_utils.dart
+++ lib/presentation/dashboard/utils/dashboard_utils.dart
@@ -5,7 +5,7 @@ import 'package:budgetapp/domain/models/transaction/transaction_model.dart';
 
 Map<int, List<TransactionModel>> mapTransactionToParentTag(
     {required List<TransactionModel> transactionList,
-    TransactionType transactionType = TransactionType.income}) {
+    required TransactionType transactionType}) {
   final Map<int, List<TransactionModel>> mapTransactionToParentTag = {};
 
   for (var transaction in transactionList) {
@@ -21,9 +21,10 @@ Map<int, List<TransactionModel>> mapTransactionToParentTag(
 
 Map<int, double> getTotalTransactionByParentGroup(
     {required List<TransactionModel> transactionList,
-    TransactionType transactionType = TransactionType.income}) {
+    required TransactionType transactionType}) {
   Map<int, double> result = {};
-  mapTransactionToParentTag(transactionList: transactionList)
+  mapTransactionToParentTag(
+          transactionList: transactionList, transactionType: transactionType)
       .forEach((key, value) {
     for (var element in value) {
       double currentTotal = result[key] ?? 0;
